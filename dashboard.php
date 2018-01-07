f168734462caefbd9b41cdce3e3219c67818c4c1
janjan <janjantandayag123@gmail.com>
2018-01-08 00:31:08 +0800

    Initial files

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php
+  include('includes/header.php');
+  include('includes/sidebar.php');
+  include('includes/top_navigation.php');
+?>
+  <div class="right_col" role="main">
+    <!-- page content -->
+  </div>
+
+<?php
+  include('includes/footer.php');
+
+       
